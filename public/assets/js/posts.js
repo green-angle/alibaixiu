4a01bda5311ff304eacbd8db82a78202bb93fed5
milier <1770724414@qq.com>
2019-10-24 21:23:37 +0800

    分页1.0

--- public/assets/js/posts.js
+++ public/assets/js/posts.js
@@ -1,10 +1,29 @@
 $.ajax({
     type: "get",
     url: "/posts",
-    success: function (response) {
-        
+    success: function (response) { 
         var html=template('postsTpl',response)
         $('#postsTbody').html(html)
-        
+        var pages=template('pageTpl',response)
+        $('#pages').html(pages)
     }
 });
+function formateDate(date){
+    date=new Date(date);
+    return date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate()
+}
+function changePage(page){
+    $.ajax({
+        type: "get",
+        url: "/posts",
+        data:{
+            page:page
+        },
+        success: function (response) { 
+            var html=template('postsTpl',response)
+            $('#postsTbody').html(html)
+            var pages=template('pageTpl',response)
+            $('#pages').html(pages)
+        }
+    });
+}
