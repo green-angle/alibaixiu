9b8f384c2a5fae2ebed03deed42cba64c0835a2a
milier <1770724414@qq.com>
2019-10-25 14:07:44 +0800

    文章列表显示1.0

--- public/assets/js/posts.js
+++ public/assets/js/posts.js
@@ -27,3 +27,29 @@ function changePage(page){
         }
     });
 }
+
+$.ajax({
+    type: "get",
+    url: "/categories",
+    success: function (response) {
+        console.log(response);
+        var html=template('fenleiTpl',{data:response})
+       $('#fenlei').html(html)
+    }
+});
+$('#shaixuanForm').on('submit',function(){
+    // alert(1)
+    var formData=$(this).serialize()
+    $.ajax({
+        type: "get",
+        url: "/posts",
+        data:formData,
+        success: function (response) { 
+            var html=template('postsTpl',response)
+            $('#postsTbody').html(html)
+            var pages=template('pageTpl',response)
+            $('#pages').html(pages)
+        }
+    });
+    return false;
+})
